3cf56975ad0945d0fbc7adff10bb99fd00249c0c
renyuan <hry87625@gmail.com>
2022-03-31 10:45:50 +0200

    add example project code

---
+++ project-dev/example project/PrimeChecker.hpp
@@ -0,0 +1,12 @@
+#ifndef PRIMECHECKER
+#define PRIMECHECKER
+#include <cstdint>
+class PrimeChecker
+{
+private:
+    /* data */
+public:
+    bool isPrime(uint16_t n);
+};
+
+#endif
---
+++ project-dev/example project/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "PrimeChecker.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        /* code */
+        int number = std::stoi(argv[1]);
+        PrimeChecker pc;
+        std::cout << "Renyuan, Huang;" << number << " is a prime number? " << pc.isPrime(number) << std::endl;
+    }
+
+    return 0;
+}
