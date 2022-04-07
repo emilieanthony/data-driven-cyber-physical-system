f9e00b4d41ac9c2f58f65ffb59be9dfc2ae80b56
renyuan <hry87625@gmail.com>
2022-04-07 11:41:08 +0200

    using example code as starter update docker file and code

--- project-dev/src/helloworld.cpp
+++ project-dev/src/helloworld.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "PrimeChecker.hpp"
 
 int main(int argc, char **argv)
 {
+    if (argc == 2)
+    {
+        /* code */
+        int number = std::stoi(argv[1]);
+        PrimeChecker pc;
+        std::cout << "firstname, lastname;" << number << " is a prime number? " << pc.isPrime(number) << std::endl;
+    }
 
-    std::cout << "hello world" << std::endl;
     return 0;
 }
